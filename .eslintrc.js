55c43282c7fc9a851f9c61772df856855f5678e0
iMocco <taoxin167@gmail.com>
2017-09-12 17:31:33 +0800

    引入iview

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,9 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'spaced-comment': [2, "always"],
+    'indent': 0,
+    'no-tabs': 0
   }
 }
